31ef5898cf655882fa051959348610c223d4df0c
thursday <xthursdayx@mailbox.org>
2023-02-13 07:40:42 +0000

    Update 04-create-services.sh

--- rootfs/etc/cont-init.d/04-create-services.sh
+++ rootfs/etc/cont-init.d/04-create-services.sh
@@ -23,8 +23,8 @@ mkdir -p /etc/services.d/rtorrent
 cat > /etc/services.d/rtorrent/run <<EOL
 #!/usr/bin/execlineb -P
 with-contenv
-/bin/export HOME /data/rtorrent
-/bin/export PWD /data/rtorrent
+/bin/export HOME /config/rtorrent
+/bin/export PWD /config/rtorrent
 s6-setuidgid ${PUID}:${PGID}
 EOL
 if [ -z "${WAN_IP}" ]; then
